bf4cc84ec05522ff0a468ebac613302b4d1c83e7
Kieran Sagar Parikh <kieran@parikh.ca>
2023-10-12 23:20:40 -0400

    tutorial pt6

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,29 +1,37 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+                data.allMdx.nodes.map(node => (
+                    <article key={node.id}>
+                        <h2>
+                            <Link to={`/${node.frontmatter.slug}`}>
+                                {node.frontmatter.title}
+                            </Link>
+                        </h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
+        }
+        id
       }
     }
   }
