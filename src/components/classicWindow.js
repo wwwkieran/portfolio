ee8e0724f1abe70433607cd2f45663d7d28e55ea
Kieran Sagar Parikh <kieran@parikh.ca>
2023-10-24 23:36:05 -0400

    improve classic window

--- src/components/classicWindow.js
+++ src/components/classicWindow.js
@@ -1,9 +1,9 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { window, windowHeader, windowHeaderButton, windowHeaderSpacer, windowHeaderText, windowContent} from './classicWindow.module.scss'
+import { window, windowHeader, windowHeaderButton, windowHeaderSpacer, windowHeaderText, windowContent, minimizeButtonInternalOne, minimizeButtonInternalTwo, resizeButtonInternal } from './classicWindow.module.scss'
 
 const ClassicWindow = ({ windowTitle, children }) => {
-    return (
+  return (
         <div className={window} id="window">
             <div className={windowHeader} id="windowHeader">
                 <button className={windowHeaderButton} id="closeButton" ></button>
@@ -11,20 +11,19 @@ const ClassicWindow = ({ windowTitle, children }) => {
                     <p className={windowHeaderText}>{windowTitle}</p>
                     <div className={windowHeaderSpacer}></div>
                     <button className={windowHeaderButton} id="resizeButton" >
-                        {/*<div*/}
-                        {/*    style="border:none; border-right: 1px solid black; border-bottom: 1px solid black; margin-top: -5px; box-shadow: none; width:6px; height: 6px;">*/}
-                        {/*</div>*/}
+                         <div className={resizeButtonInternal}>
+                         </div>
                     </button>
                     <button className={windowHeaderButton} id="minimizeButton" >
-                        {/*<div style="border:none; border-top: 1px solid black; margin-top: 0px; box-shadow: none;"></div>*/}
-                        {/*<div style="border:none; border-top: 1px solid black; margin-top: 1px; box-shadow: none;"></div>*/}
+                        <div className={minimizeButtonInternalOne}></div>
+                        <div className={minimizeButtonInternalTwo}></div>
                     </button>
             </div>
             <div className={windowContent} id="windowContent">
                 {children}
             </div>
         </div>
-    )
+  )
 }
 
 export default ClassicWindow
