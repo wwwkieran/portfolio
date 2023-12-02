9cfde89f2c07b855522595d0a8dc1d7a467da2a1
Kieran Sagar Parikh <kieran@parikh.ca>
2023-12-01 23:20:10 -0500

    hero vid for gen lino

--- src/components/projectHero.js
+++ src/components/projectHero.js
@@ -2,14 +2,26 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 // import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import { container, metadataContainer, description, imageWindow, imageContainer, seemoreText, textContainer, spacer, heroVideo} from './projectHero.module.scss'
+import {
+  container,
+  metadataContainer,
+  description,
+  imageWindow,
+  imageContainer,
+  seemoreText,
+  textContainer,
+  spacer,
+  heroVideo
+} from './projectHero.module.scss'
 import ClassicWindow from './classicWindow'
 import PixelPainter from '../images/pixel_painter.mp4'
 import QuickSettings from '../pages/projects/quicksettings-iterator/hierarchical.mp4'
+import GenLino from '../pages/projects/generative-prints/hero-vid.mp4'
 
 const videoMap = {
-    "PixelPainter": PixelPainter,
-    "QuickSettings": QuickSettings,
+  PixelPainter,
+  QuickSettings,
+  GenLino
 }
 
 // eslint-disable-next-line react/prop-types
@@ -19,24 +31,24 @@ const ProjectHero = ({ node }) => {
   return (
         <div className={container}>
             <div className={textContainer}>
-                <ClassicWindow windowTitle={"  " + node.frontmatter.title + " "}>
+                <ClassicWindow windowTitle={'  ' + node.frontmatter.title + ' '}>
                     <div className={metadataContainer}>
                         <h6 style={{ marginRight: 'auto' }}>{node.frontmatter.date}</h6>
-                        {/*<h6 style={{ marginLeft: 'auto' }}>{node.frontmatter.date}</h6>*/}
+                        {/* <h6 style={{ marginLeft: 'auto' }}>{node.frontmatter.date}</h6> */}
                     </div>
                 <h3 className={description}>{node.frontmatter.short_description}</h3>
                         <ClassicWindow windowTitle="Preview" style={{
-                            width: "80%",
-                            margin: "10px",
-                            marginLeft: "auto",
+                          width: '80%',
+                          margin: '10px',
+                          marginLeft: 'auto'
                         }}>
-                            {node.frontmatter.hero_video && <video className={heroVideo} autoPlay={"true"} loop={"true"} preload={"auto"} muted={"true"}><source src={videoMap[node.frontmatter.hero_video]} type="video/mp4" /></video>}
+                            {node.frontmatter.hero_video && <video className={heroVideo} autoPlay={'true'} loop={'true'} preload={'auto'} muted={'true'}><source src={videoMap[node.frontmatter.hero_video]} type="video/mp4" /></video>}
                             {node.frontmatter.hero_image && <GatsbyImage alt={node.frontmatter.hero_image_alt} image={getImage(node.frontmatter.hero_image)} />}
                         </ClassicWindow>
-            {/*<div style={{ display: 'flex' }}>*/}
-            {/*    <div style={{ width: "100%" }}></div>*/}
-            {/* <Link to={`/${node.frontmatter.slug}`} className={seemoreText}>See More --> </Link>*/}
-            {/*</div>*/}
+            {/* <div style={{ display: 'flex' }}> */}
+            {/*    <div style={{ width: "100%" }}></div> */}
+            {/* <Link to={`/${node.frontmatter.slug}`} className={seemoreText}>See More --> </Link> */}
+            {/* </div> */}
                 </ClassicWindow>
             </div>
             {/* <hr/> */}
