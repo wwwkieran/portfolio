0f5377b1b6821157bcd131e0811a64c2d81f7614
Kieran Sagar Parikh <kieran@parikh.ca>
2024-03-29 17:08:16 -0400

    menu bar themes

--- src/components/menuBar.js
+++ src/components/menuBar.js
@@ -1,11 +1,17 @@
 import * as React from 'react'
 import { PropTypes } from 'prop-types'
-import { menuBar, menuBarElement, menuBarSpacer } from './menuBar.module.scss'
+import {
+  menuBar,
+  menuBarClassic,
+  menuBarLoading,
+  menuBarElement,
+  menuBarSpacer
+} from './menuBar.module.scss'
 import { Link } from 'gatsby'
 
 const MenuBar = ({ buttons }) => {
   return (
-      <div className={menuBar}>
+      <div className={menuBarClassic}>
           <div className={menuBarElement} style={{ fontSize: '1.1em', paddingLeft: '10px' }} onTouchStart>&#63743;</div>
           {
               buttons !== undefined && buttons.map((v) => {
