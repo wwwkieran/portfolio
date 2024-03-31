1b186a17d9a240aa5df7cac627733ac54b7e764d
Kieran Sagar Parikh <kieran@parikh.ca>
2024-03-31 17:01:34 -0400

    working transition animation

--- src/components/navBarLayout.js
+++ src/components/navBarLayout.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import MenuBar from './menuBar'
 import { globalContainer, pageContainer } from './navBarLayout.module.scss'
+import { AnimatePresence } from 'framer-motion'
+
 const NavBarLayout = ({ children }) => {
   return (<div className={globalContainer}>
         <MenuBar buttons={[['Home', '/desktop'], ['Work', '/work'], ['Blog', '/blog'], ['About', '/about'], ['Shop', '/shop']]}/>
-        <div className={pageContainer}>{children}</div>
+      <div className={pageContainer}><AnimatePresence mode={'wait'}>{children}</AnimatePresence></div>
         </div>)
 }
 
--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -1,17 +1,22 @@
 import * as React from 'react'
 import Seo from '../components/seo'
 import ClassicWindow from '../components/classicWindow'
+import { motion } from 'framer-motion'
 
 const DesktopPage = () => {
   return (
-      <div style={{ backgroundColor: '#9899CC', position: 'absolute', top: '27px', left: '0px', bottom: '0px', right: '0px', zIndex: '-1' }}>
-     <ClassicWindow windowTitle={'Introduction'} width={'400px'} y={'100px'} x={'100px'}>
-         <h1>Hi, I’m Kieran!</h1>
-         <br/>
-         <br/>
-         I’m a software engineer and digital artist interested in the future of digital creativity tools.
+      <div style={{ backgroundColor: '#9899CC', position: 'absolute', top: '27px', left: '0px', right: '0px', bottom: '0px', zIndex: '-1' }} >
+         <ClassicWindow windowTitle={'Introduction'} width={'400px'} y={'100px'} x={'100px'}>
+             <h1>Hi, I’m Kieran!</h1>
+             <br/>
+             <br/>
+             I’m a software engineer and digital artist interested in the future of digital creativity tools.
 
-     </ClassicWindow>
+         </ClassicWindow>
+          <motion.div style={{ backgroundColor: '#000000', position: 'absolute', top: '0px', left: '0px', right: '0px', bottom: '0px', zIndex: '1' }} animate={{ top: '100vh' }} exit={{ top: '0px' }} transition={{ ease: 'easeInOut', duration: 0.7 }}>
+
+          </motion.div>
+
       </div>
   )
 }
