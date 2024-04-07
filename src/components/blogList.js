d4d6254227c4dce27a590661be53b38c0667432f
Kieran Parikh <kieran@parikh.ca>
2024-04-07 18:25:37 -0400

    blog index page working

--- src/components/blogList.js
+++ src/components/blogList.js
@@ -1,12 +1,37 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { container, yearContainer, year, posts, link } from './blogList.module.scss'
+import { graphql, Link, useStaticQuery } from 'gatsby'
+import {
+  container,
+  yearContainer,
+  year,
+  posts,
+  link
+} from './blogList.module.scss'
 
 const BlogList = () => {
+  const data = useStaticQuery(graphql`
+      query {
+  allMdx(filter: {internal: {contentFilePath: {regex: "/blog/"}}}) {
+    nodes {
+      frontmatter {
+        title
+        slug
+      }
+      internal {
+        contentFilePath
+      }
+      id
+    }
+  }
+}
+    `)
   return (
         <div className={container}>
             <div className={yearContainer}>
-                <p className={year}> 2024 </p>  <p className={posts}> / <Link className={link} to={'/'}>How I built this site</Link> / <Link className={link} to={'/'}>Another post</Link></p>
+                <p className={year}> 2024 </p>
+                { data.allMdx.nodes.map(node => (
+                    <p className={posts} key={node.frontmatter.slug}> / <Link className={link} to={'/blog/' + node.frontmatter.slug}>{node.frontmatter.title}</Link></p>
+                ))}
             </div>
         </div>
   )
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,13 +4,15 @@ import { motion } from 'framer-motion'
 import BlogList from '../components/blogList'
 
 const BlogPage = () => {
-  return (<motion.div initial={{ opacity: 0, translateX: -3 }} animate={{ opacity: 1, translateX: 0 }} exit={{ opacity: 0 }} transition={{ duration: 0.3 }}>
-            <h1 style={{ color: 'white' }}>Blog</h1>
-            <h1 style={{ color: 'white' }}>Blog</h1>
-            <h1 style={{ color: 'white' }}>Blog</h1>
-            <h1 style={{ color: 'white' }}>Blog</h1>
-            <h1 style={{ color: 'white' }}>Blog</h1>
-          <div>
+  return (<motion.div initial={{ opacity: 0, translateX: -3 }} style={{ display: 'flex' }} animate={{ opacity: 1, translateX: 0 }} exit={{ opacity: 0 }} transition={{ duration: 0.3 }}>
+          <div style={{ display: 'inline' }}>
+              <h1 style={{ color: 'white' }}>Blog</h1>
+              <h1 style={{ color: 'white' }}>Blog</h1>
+              <h1 style={{ color: 'white' }}>Blog</h1>
+              <h1 style={{ color: 'white' }}>Blog</h1>
+              <h1 style={{ color: 'white' }}>Blog</h1>
+          </div>
+          <div style={{ marginLeft: '10%', marginRight: 'auto', marginTop: 'auto', marginBottom: '0' }}>
               <BlogList/>
           </div>
         </motion.div>
