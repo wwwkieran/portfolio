ac5280bd182b9b394b7e850fbf3ca6ad62dbe1a3
Kieran Sagar Parikh <kieran@parikh.ca>
2024-06-12 17:14:56 -0400

    ani gallery working but broken

--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -4,11 +4,13 @@ import ClassicWindow from '../components/classicWindow'
 import { motion } from 'framer-motion'
 import IconLink from '../components/desktop/iconLink'
 import { useSiteMetadata } from '../components/hooks/siteMetadata'
+import { GatsbyImage, StaticImage } from 'gatsby-plugin-image'
+import { AnimatedGallery } from '../components/desktop/aniGallery'
 
 const DesktopPage = () => {
-  const metadata = useSiteMetadata()
   return (
       <div style={{ backgroundColor: '#9899CC', position: 'absolute', top: '27px', left: '0px', right: '0px', bottom: '0px', zIndex: '-1' }} >
+          <GalleryWindow x={'450px'} y={'130px'}/>
          <ClassicWindow windowTitle={'Introduction'} width={'320px'} y={'100px'} x={'100px'}>
              <div style={{ margin: '10px' }}>
 
@@ -20,37 +22,51 @@ const DesktopPage = () => {
                  Exploring algorithms, robotics, and patterns.</p>
              </div>
          </ClassicWindow>
-          <ClassicWindow windowTitle={'Links'} width={'300px'} y={'400px'} x={'150px'}>
-              <div style={{ margin: '20px' }}>
-                  <p style={{
-                    fontFamily: 'Geneva, sans-serif',
-                    fontSize: '0.9em',
-                    fontSmooth: 'never',
-                    webkitFontSmoothing: 'none',
-                    color: 'black'
-                  }}>Links:</p>
-                  <div style={{
-                    display: 'grid',
-                    gridTemplateColumns: 'repeat(4, 1fr)',
-                    gap: '10px',
-                    gridAutoRows: '70px'
-                  }}>
-                      <IconLink link='/work' description={'Work'} imagePath={'/images/book.png'}/>
-                      <IconLink link={metadata.instagramUrl} description={'Instagram'} imagePath={'/images/insta.png'}/>
-                      <IconLink link={metadata.githubUrl} description={'Github'} imagePath={'/images/github.png'}/>
-                      <IconLink link={metadata.tiktokUrl} description={'TikTok'} imagePath={'/images/tiktok.png'}/>
-                      <IconLink link={metadata.linkedinUrl} description={'Linkedin'}
-                                imagePath={'/images/linkedin.png'}/>
-                  </div>
-                  <br/>
-              </div>
-          </ClassicWindow>
-          <ClassicWindow windowTitle={'Gallery'} width={'300px'} y={'450px'} x={'250px'}>
-          </ClassicWindow>
+          <LinksWindow x={'200px'} y={'250px'}/>
           <motion.div style={{ backgroundColor: '#000000', position: 'absolute', top: '0px', left: '0px', right: '0px', bottom: '0px', zIndex: '1' }} animate={{ top: '100vh' }} exit={{ top: '0px' }} transition={{ ease: 'easeInOut', duration: 0.7 }}>
           </motion.div>
       </div>
   )
+}
+
+const LinksWindow = ({ x, y }) => {
+  const metadata = useSiteMetadata()
+  return (
+        <ClassicWindow windowTitle={'Links'} width={'fit-content'} y={y} x={x}>
+            <div style={{ margin: '10px' }}>
+                {/* <p style={{ */}
+                {/*  fontFamily: 'Geneva, sans-serif', */}
+                {/*  fontSize: '0.9em', */}
+                {/*  fontSmooth: 'never', */}
+                {/*  webkitFontSmoothing: 'none', */}
+                {/*  color: 'black' */}
+                {/* }}>Links:</p> */}
+                <div style={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(3, 1fr)',
+                  gap: '10px',
+                  gridAutoRows: '65px',
+                  marginLeft: 'auto',
+                  marginRight: 'auto'
+                }}>
+                    <IconLink link='/work' description={'Work'} imagePath={'/images/book.png'}/>
+                    <IconLink link='/blog' description={'Blog'} imagePath={'/images/book.png'}/>
+                    <IconLink link={metadata.instagramUrl} description={'Instagram'} imagePath={'/images/insta.png'}/>
+                    <IconLink link={metadata.githubUrl} description={'Github'} imagePath={'/images/github.png'}/>
+                    <IconLink link={metadata.tiktokUrl} description={'TikTok'} imagePath={'/images/tiktok.png'}/>
+                    <IconLink link={metadata.linkedinUrl} description={'Linkedin'}
+                              imagePath={'/images/linkedin.png'}/>
+                </div>
+                <br/>
+            </div>
+        </ClassicWindow>
+  )
+}
+
+const GalleryWindow = ({ x, y }) => {
+  return (<ClassicWindow windowTitle={'Gallery'} width={'400px'} y={y} x={x}>
+      <AnimatedGallery images={['../images/IMG_8766.jpeg']}/>
+    </ClassicWindow>)
 }
 
 export const Head = () => <><Seo title="Home" /></>
