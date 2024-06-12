ff2f303173fe8e4be5fdac8595217c663208676e
Kieran Sagar Parikh <kieran@parikh.ca>
2024-06-11 23:30:49 -0400

    modify desktop

--- src/pages/desktop.js
+++ src/pages/desktop.js
@@ -9,27 +9,42 @@ const DesktopPage = () => {
   const metadata = useSiteMetadata()
   return (
       <div style={{ backgroundColor: '#9899CC', position: 'absolute', top: '27px', left: '0px', right: '0px', bottom: '0px', zIndex: '-1' }} >
-         <ClassicWindow windowTitle={'Introduction'} width={'400px'} y={'100px'} x={'100px'}>
-             <div style={{ margin: '20px' }}>
+         <ClassicWindow windowTitle={'Introduction'} width={'320px'} y={'100px'} x={'100px'}>
+             <div style={{ margin: '10px' }}>
 
              <h2 style={{ marginTop: '0px', marginBottom: '0px', fontFamily: 'Charcoal, Geneva, sans-serif', color: 'black' }}>
                  wwwkieran
              </h2>
-             <p style={{ fontSize: '1.0em', fontFamily: 'Geneva, sans-serif', color: 'black', fontSmooth: 'never', webkitFontSmoothing: 'none', marginTop: '0px', marginLeft: '5px', marginRight: '5px', marginBottom: '50px' }}>
+             <p style={{ fontSize: '1.0em', fontFamily: 'Geneva, sans-serif', color: 'black', fontSmooth: 'never', webkitFontSmoothing: 'none', marginTop: '0px', marginLeft: '5px', marginRight: '5px', marginBottom: '10px' }}>
                  Visual Artist + Computer Engineer.
                  Exploring algorithms, robotics, and patterns.</p>
-
-             <p style={{ fontFamily: 'Geneva, sans-serif', fontSize: '0.9em', fontSmooth: 'never', webkitFontSmoothing: 'none', color: 'black' }}>Links:</p>
-             <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '10px', gridAutoRows: '70px' }}>
-                 <IconLink link='/work' description={'Work'} imagePath={'/images/book.png'}/>
-                 <IconLink link={metadata.instagramUrl} description={'Instagram'} imagePath={'/images/insta.png'}/>
-                 <IconLink link={metadata.githubUrl} description={'Github'} imagePath={'/images/github.png'}/>
-                 <IconLink link={metadata.tiktokUrl} description={'TikTok'} imagePath={'/images/tiktok.png'}/>
-                 <IconLink link={metadata.linkedinUrl} description={'Linkedin'} imagePath={'/images/linkedin.png'}/>
-             </div>
-             <br/>
              </div>
          </ClassicWindow>
+          <ClassicWindow windowTitle={'Links'} width={'300px'} y={'400px'} x={'150px'}>
+              <div style={{ margin: '20px' }}>
+                  <p style={{
+                    fontFamily: 'Geneva, sans-serif',
+                    fontSize: '0.9em',
+                    fontSmooth: 'never',
+                    webkitFontSmoothing: 'none',
+                    color: 'black'
+                  }}>Links:</p>
+                  <div style={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(4, 1fr)',
+                    gap: '10px',
+                    gridAutoRows: '70px'
+                  }}>
+                      <IconLink link='/work' description={'Work'} imagePath={'/images/book.png'}/>
+                      <IconLink link={metadata.instagramUrl} description={'Instagram'} imagePath={'/images/insta.png'}/>
+                      <IconLink link={metadata.githubUrl} description={'Github'} imagePath={'/images/github.png'}/>
+                      <IconLink link={metadata.tiktokUrl} description={'TikTok'} imagePath={'/images/tiktok.png'}/>
+                      <IconLink link={metadata.linkedinUrl} description={'Linkedin'}
+                                imagePath={'/images/linkedin.png'}/>
+                  </div>
+                  <br/>
+              </div>
+          </ClassicWindow>
           <ClassicWindow windowTitle={'Gallery'} width={'300px'} y={'450px'} x={'250px'}>
           </ClassicWindow>
           <motion.div style={{ backgroundColor: '#000000', position: 'absolute', top: '0px', left: '0px', right: '0px', bottom: '0px', zIndex: '1' }} animate={{ top: '100vh' }} exit={{ top: '0px' }} transition={{ ease: 'easeInOut', duration: 0.7 }}>
